62e75bc3ba5fc304858e48d5b2f1a0b180e66548
Sanskriti Agrawal <127824536+sanskritiagrawal28@users.noreply.github.com>
2023-03-22 10:32:59 +0530

    Add files via upload

---
+++ td1.py
@@ -0,0 +1,35 @@
+import turtle
+for x in range(10):
+	turtle.forward(x*10)
+	turtle.left(90)
+	
+======================================================================================
+
+import turtle
+colors=["blue", "red", "green", "pink", "orange", "purple", "yellow"]
+
+t= turtle.Pen()
+turtle.bgcolor("black")
+
+for x in range(360):
+	t.pencolor(colors[x%7])
+	t.width(x/10 +1)
+	t.forward(x*2)
+	t.left(90)
+
+======================================================================================
+
+import turtle
+
+p= turtle.Turtle()
+p.color("blue")
+def func(size):
+	for x in range(14):
+		p.fd(size)
+		p.left(90)
+		size=size+15
+func(6)
+func(36)
+func(42)
+func(56)
+
